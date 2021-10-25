8ca00ede54d8ffd459cee8f5022c43de7a39dbd7
CaptainEurope <petkovvilimir@gmail.com>
2021-10-25 23:04:06 +0300

    Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHi(string name, int age);
+
+int main()
+{
+    sayHi ("V", 25);
+    sayHi ("Tom", 20);
+    sayHi ("Mike", 30);
+
+    return 0;
+}
+void sayHi(string name, int age)
+{
+    cout << "Hello " << name << ", you are " << age << " years old." << endl;
+}
